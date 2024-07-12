789aa05fc1dee0b584ad14dc9c773619d55a289f
Etienne <v.etienne07@gmail.com>
2024-07-12 14:07:10 -0400

    Add links and manage notfound pages

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -1,12 +1,18 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import { Route, 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout';
 import HomePage from './Pages/HomePage';
-
+import JobsPage from './Pages/JobsPage';
+import NotFoundPage from './Pages/NotFoundPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage/>}/>
+      <Route path='*' element={<NotFoundPage/>}/>
     </Route>)
 );
 
