d773fb6d7e8ebaffda891d3a3a2e87a252bb1574
Etienne <v.etienne07@gmail.com>
2024-07-18 22:33:20 -0400

    Finishing JobPage and started AddJobPage

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -6,14 +6,16 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './Pages/HomePage';
 import JobsPage from './Pages/JobsPage';
 import NotFoundPage from './Pages/NotFoundPage';
-import JobPage from './Pages/JobPage';
+import JobPage,{jobLoader} from './Pages/JobPage';
+import AddJobPage from './Pages/AddJobPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage/>}/>
-      <Route path='/job/:id' element={<JobPage/>}/>
+      <Route path='/add-Job' element={<AddJobPage/>}/>
+      <Route path='/job/:id' element={<JobPage/>} loader ={jobLoader}/>
       <Route path='*' element={<NotFoundPage/>}/>
     </Route>)
 );
