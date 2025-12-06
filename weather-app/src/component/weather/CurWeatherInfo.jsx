d2c06e1cf4322e4dbcef660551ecdc9e34af23d1
EtienneVaillancourt <v.etienne07@gmail.com>
2025-12-06 14:59:34 -0500

    Finishing general left panel setup

--- weather-app/src/component/weather/CurWeatherInfo.jsx
+++ weather-app/src/component/weather/CurWeatherInfo.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import WeatherImage from '../common/WeatherImage'
+import cloudImage from "../../assets/image/cloud.svg"
 
 export default function CurWeatherInfo(){
   return (
-    <div>CurWeatherInfo</div>
+    <div className='w-full flex items-center flex-col'>
+      <h1>12 Deg</h1>
+      <div className='flex flex-row'>
+        <h2>monday,</h2>
+        <p>16:00</p>
+      </div>
+      <hr className='h-px bg-neutral-400 border-0 w-3/4'/>
+      <div className='flex justify-center items-center gap-2'>
+        <WeatherImage imgSource={cloudImage} className="w-4 h-4"/>
+        <h3>Mostly Cloudy</h3>
+      </div>
+        <div className='flex justify-center items-center gap-2'>
+        <WeatherImage imgSource={cloudImage} className="w-4 h-4"/>
+        <h3>Rain - 30%</h3>
+      </div>
+    </div>
   )
 }
--- weather-app/src/component/weather/SearchPlace.jsx
+++ weather-app/src/component/weather/SearchPlace.jsx
@@ -3,11 +3,11 @@ import locationIcon from "../../assets/icon/location.svg";
 
 export default function SearchPlace() {
   return (
-    <div className="flex">
-      <form action="">
-        <input type="text" placeholder="Search for places..."></input>
-        <button>
-          <img src={locationIcon} alt="loc" className="w-6 h-6 bg-gray-200 rounded-full" />
+    <div className="mt-4 w-full flex justify-center">
+      <form className="flex items-center justify-center gap-2 w-3/4 max-w-56" action="">
+        <input type="text" placeholder="Search for places..." className="bg-transparent w-3/4"></input>
+        <button className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center">
+          <img src={locationIcon} alt="loc" className="w-4 h-4" />
         </button>
       </form>
     </div>
